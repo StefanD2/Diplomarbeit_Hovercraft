adcfec4d045df057a3b6422a52d20a6904029dff
Philipp Eilmsteiner <philipp@eilm.at>
2020-10-26 19:04:56 +0100

    GasRegelung über CANBUS + PWM Erzeugung funktionsfähig

--- Regler_Controller/src/main.cpp
+++ Regler_Controller/src/main.cpp
@@ -1,32 +1,39 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <mcp2515.h>
-struct can_frame canMsg1;
-MCP2515 mcp2515(10);
+
+#define REGLER_CAN_ID 0xF7
+
+struct can_frame canMsg;
+MCP2515 mcp2515(7);
 
 
 void setup() {
-  canMsg1.can_id  = 0x0F6;
-  canMsg1.can_dlc = 2;
-  canMsg1.data[0] = 0x8E;
-  canMsg1.data[1] = 0x87;
-
-  
-  while (!Serial);
   Serial.begin(115200);
+  DDRB=DDRB|0B10;
+  TCCR1A=0b10<<COM1A0|0b10<<WGM10;
+  TCCR1B=0b11<<WGM12|0b11<<CS10;
+  //TIMSK1=0b10|1<<ICIE1;
+
+  ICR1H=0B10000;
+  ICR1L=0B0;
+  
+  sei();
   
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
   mcp2515.setNormalMode();
-  
-  Serial.println("Example: Write to CAN");
+
+  OCR1AH=0b01;
+  OCR1AL=0;
 }
 
 void loop() {
-  mcp2515.sendMessage(&canMsg1);
-  mcp2515.sendMessage(&canMsg2);
-
-  Serial.println("Messages sent");
-  
-  delay(100);
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    if ((canMsg.can_id==REGLER_CAN_ID)&&(canMsg.can_dlc==2)&&(canMsg.data[0]==0x00)){
+        char value = canMsg.data[1];
+        OCR1AH=0b01;
+        OCR1AL=value;
+    }
+  }
 }
