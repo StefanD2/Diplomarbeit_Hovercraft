b9a873423226e84ed215f2f6872241cfbb85553d
Philipp Eilmsteiner <philipp@eilm.at>
2020-10-26 16:53:33 +0100

    added Regler_Controller

---
+++ Regler_Controller/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <mcp2515.h>
+struct can_frame canMsg1;
+MCP2515 mcp2515(10);
+
+
+void setup() {
+  canMsg1.can_id  = 0x0F6;
+  canMsg1.can_dlc = 2;
+  canMsg1.data[0] = 0x8E;
+  canMsg1.data[1] = 0x87;
+
+  
+  while (!Serial);
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+  Serial.println("Example: Write to CAN");
+}
+
+void loop() {
+  mcp2515.sendMessage(&canMsg1);
+  mcp2515.sendMessage(&canMsg2);
+
+  Serial.println("Messages sent");
+  
+  delay(100);
+}
